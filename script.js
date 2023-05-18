cd8aa6d29effb905453ea5641fe5e072f43ae4f0
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-18 16:48:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeItems() {
+	let selectedOption = document.getElementById('colorSelect').options[document.getElementById('colorSelect').selectedIndex];
+
+	selectedOption.remove();
+
+}
+
+document.getElementById('removeBtn').addEventListner('click', removeItems);
